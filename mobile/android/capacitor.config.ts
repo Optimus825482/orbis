676e0dd56d6be4737e7cd235e90274396a7da10e
optmus825454 <ikinciyenikitap54@gmail.com>
2026-01-15 14:54:35 +0300

    HATALAR GIDERILDI1

---
+++ mobile/android/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.orbis.app',
+  appName: 'orbis',
+  webDir: 'orbis.erkanerdem.net'
+};
+
+export default config;
